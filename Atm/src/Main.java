204c13ee1cf6be175c65800e64a2c25da1b50f6f
mohanapriya.v <100412589+mohanapriya-2409@users.noreply.github.com>
2024-02-03 22:56:39 +0530

    Delete Atm directory

--- Atm/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BankAccount account = new BankAccount(1000);
-        Atm atm = new Atm(account);
-        System.out.println("Thank You!!");
-    }
-}
